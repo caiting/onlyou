0833ff9ffaa2eba32c9e4fa106336b633d717504
tt <jenny.pei@yunzhihui.com>
2016-01-29 17:33:03 +0800

    less

---
+++ toushibao/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    uglify = require("gulp-uglify"),
+    concat = require("gulp-concat"),
+    rename = require("gulp-rename"),
+    less   = require("gulp-less");
+
+
+gulp.task('concat', function () {
+    gulp.src('app/src/js/*.js')//要合并的文件
+        .pipe(uglify())
+        .pipe(concat('lib.js'))// 合并匹配到的js文件并命名为 "lib.js"
+        .pipe(gulp.dest('dist/js'))
+        .pipe(uglify())
+        .pipe(rename('lib.min.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+gulp.task('compile-less', function () {
+    gulp.src('less/tsb.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('default',['concat','compile-less']);
+
+
+
+
+
+
+
